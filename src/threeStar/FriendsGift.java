0545ee2ba11939ffdb391b5b87379312e1fea09c
zhuxianghua <zhuxianghua@8chedao.com>
2014-01-21 00:21:50 +0800

    friends gift completed

--- src/threeStar/FriendsGift.java
+++ src/threeStar/FriendsGift.java
@@ -1,50 +1,87 @@
 package threeStar;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.math.RoundingMode;
 
 public class FriendsGift {
 
 	public static void main(String[] args) {
-		BigInteger b100 = jc(100);
-		BigInteger b51 = jc(51);
-		BigInteger b49 = jc(49);
-		
-		System.out.println(b100.divide(b51).divide(b49));
+//		BigInteger b100 = jc(100);
+//		BigInteger b51 = jc(51);
+//		BigInteger b49 = jc(49);
+//		
+//		System.out.println(b100.divide(b51).divide(b49));
+		long b = System.currentTimeMillis();
+		System.out.println(getGift(99, 0));
+		System.out.println(System.currentTimeMillis() - b);
 	}
 	
 	public static String getGift(int n, int m) {
 		String ret = "";
-		int z = n - m;
-		if (z == 1) return "0.00000000";
-
-		m = n - m;
-		BigInteger b0 = new BigInteger("1");
+		Integer z = m;
+		m = n - z;
+		if (m == 1) return "0.00000000";
+		BigInteger b0 = new BigInteger("0");
 		BigInteger b1 = new BigInteger("1");
 		BigInteger b2 = new BigInteger("2");
-		BigInteger[][] c = new BigInteger[m+1][m];
-		BigInteger[] jc = new BigInteger[m+1];
+		BigInteger[] jc = new BigInteger[n+1];
+		jc[0] = b1;
 		jc[1] = b1;
 		jc[2] = b2;
-		BigInteger[] a = new BigInteger[m];
+		BigInteger[] a = new BigInteger[m+1];
+		if (m == 0) {
+			a[0] = b1;
+		} else {
+		BigInteger[][] c = new BigInteger[m+1][];
+		c[2] = new BigInteger[3];
+		c[2][0] = b1;
+		c[2][1] = b2;
+		c[2][2] = b1;
 		a[1] = b0;
 		a[2] = b1;
 		
-		int j = m / 2;
-		for (int i = 3; i <= m; i++) {
-			
+		// a[3]..a[m]
+		for (Integer i = 3; i <= m; i++) {
+			jc[i] = jc[i-1].multiply(new BigInteger(i.toString()));
+			BigInteger tmp = b1;
+			Integer j = i / 2;
+			c[i] = new BigInteger[j+1];
+			if (i % 2 == 0) {
+				if (j - 1 == 0) {
+					c[i][j] = new BigInteger(i.toString());
+				} else {
+					// c[i][j] = c[i-1][j-1]*i/j
+					c[i][j] = c[i-1][j-1].multiply(
+						new BigInteger(i.toString())).divide(
+							new BigInteger(j.toString()));
+				}
+				tmp = tmp.add(c[i][j].multiply(a[j]));
+				j -= 1;
+			}
+			for (Integer k = 1; k <= j; k++) {
+				if (k - 1 == 0) {
+					c[i][k] = new BigInteger(i.toString());
+				} else {
+					// c[i][k] = c[i-1][k-1]*i/k
+					c[i][k] = c[i-1][k-1].multiply(new BigInteger(i.toString())).divide(new BigInteger(k.toString()));
+				}
+				// c[i][k] * (a[i-k] + a[k])
+				tmp = tmp.add(c[i][k].multiply(a[i-k].add(a[k])));
+			}
+			a[i] = jc[i].subtract(tmp);
 		}
-		return ret;
-	}
-	
-	private static BigInteger jc(int k) {
-		BigInteger bk = new BigInteger(String.valueOf(k));
-		BigInteger ret = bk;
-		BigInteger b1 = new BigInteger("1");
+		}
+		// a[m]/m!(n-m)!
+		Integer t = m + 1;
+		while (t <= z) {
+			jc[t] = jc[t-1].multiply(new BigInteger(t.toString()));
+			t++;
+		}
+		BigDecimal u = new BigDecimal(a[m]);
+		BigDecimal d = new BigDecimal(jc[m].multiply(jc[z]));
 		
-		while (bk.compareTo(b1) > 0) {
-			bk = bk.subtract(b1);
-			ret = ret.multiply(bk);
-		}
+		ret = u.divide(d, 8, RoundingMode.HALF_UP).toPlainString();
 		return ret;
 	}
 }
--- src/twoStar/Plus20.java
+++ src/twoStar/Plus20.java
@@ -3,65 +3,65 @@ package twoStar;
 
 public class Plus20 {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(calculate("9c", "de5h53hi0id437bbf796ih39bff86igeii10e8i2c983eg00c9ac1a678b80h0bf3a86cf9cb2h6e8830292cef88eg3ga312c"));
-	}
-	
-	public static String calculate(String x, String y) {
-		
-		int num = '0' + 0;
-		int c = 'a' + 0;
-		
-		StringBuilder sb = new StringBuilder();
-		StringBuilder yb = new StringBuilder(y).reverse();
-		StringBuilder xb = new StringBuilder(x).reverse();
-		
-		int len = yb.length();
-		if (len < xb.length()) len = xb.length();
-		int tmp = 0;
-		int begin = 0;
-		int xn = 0;
-		int yn = 0;
-		while (begin < len) {
-			if (begin < xb.length()) {
-				xn = xb.charAt(begin);
-				if (xn < c) {
-					xn -= num; 
-				} else {
-					xn -= c - 10;
-				}
-			}
-			if (begin < yb.length()) {
-				yn = yb.charAt(begin);
-				if (yn < c) {
-					yn -= num; 
-				} else {
-					yn -= c - 10;
-				}
-			}
-			int sum = xn + yn + tmp;
-			if (sum >= 20) {
-				tmp = 1;
-				sum -= 20;
-			} else {
-				tmp = 0;
-			}
-			if (sum < 10) {
-				sb.append((char)(sum + num));
-			} else {
-				sb.append((char)(sum - 10 + c));
-			}
-			xn = 0;
-			yn = 0;
-			begin++;
-		}
-		if (tmp == 1) sb.append('1');
-		
-		return sb.reverse().toString();
-	}
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println(calculate("9c", "de5h53hi0id437bbf796ih39bff86igeii10e8i2c983eg00c9ac1a678b80h0bf3a86cf9cb2h6e8830292cef88eg3ga312c"));
+    }
+    
+    public static String calculate(String x, String y) {
+        
+        int num = '0' + 0;
+        int c = 'a' + 0;
+        
+        StringBuilder sb = new StringBuilder();
+        StringBuilder yb = new StringBuilder(y).reverse();
+        StringBuilder xb = new StringBuilder(x).reverse();
+        
+        int len = yb.length();
+        if (len < xb.length()) len = xb.length();
+        int tmp = 0;
+        int begin = 0;
+        int xn = 0;
+        int yn = 0;
+        while (begin < len) {
+            if (begin < xb.length()) {
+                xn = xb.charAt(begin);
+                if (xn < c) {
+                    xn -= num; 
+                } else {
+                    xn -= c - 10;
+                }
+            }
+            if (begin < yb.length()) {
+                yn = yb.charAt(begin);
+                if (yn < c) {
+                    yn -= num; 
+                } else {
+                    yn -= c - 10;
+                }
+            }
+            int sum = xn + yn + tmp;
+            if (sum >= 20) {
+                tmp = 1;
+                sum -= 20;
+            } else {
+                tmp = 0;
+            }
+            if (sum < 10) {
+                sb.append((char)(sum + num));
+            } else {
+                sb.append((char)(sum - 10 + c));
+            }
+            xn = 0;
+            yn = 0;
+            begin++;
+        }
+        if (tmp == 1) sb.append('1');
+        
+        return sb.reverse().toString();
+    }
 
 }
